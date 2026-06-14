f9ed05b0b48e047af30d76fb5350a28d66683544
kemamjhb025@student.wethinkcode.co.za <kemamjhb025@student.wethinkcode.co.za>
2026-06-14 17:34:48 +0200

    Add Job model class with fields and getters

--- src/za/co/jobmatchsa/model/Job.java
+++ src/za/co/jobmatchsa/model/Job.java
@@ -1,8 +1,32 @@
 package za.co.jobmatchsa.model;
 
-
 public class Job {
 
+    private String title;
+    private String skills;
+    private String location;
+    private double salary;
+
+    public Job(String title, String skills, String location, double salary) {
+        this.title = title;
+        this.skills = skills;
+        this.location = location;
+        this.salary = salary;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
 }
-
-
